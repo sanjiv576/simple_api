f84529b76b3b62dd5e1d68549d6abe44f776148b
sanjiv576 <shresthasanjiv576@gmail.com>
2023-04-16 11:47:30 +0545

    move different routes and add put and delete methods

---
+++ books_api/routes/book_routes.js
@@ -0,0 +1,96 @@
+
+const express = require('express');
+// import custom books
+
+let books = require('../data/books');
+
+// express.Router() --> is used to handle different routes moudalizer
+const router = express.Router();
+
+// this is main route for /api/books
+router.route('/')
+
+    // get all books 
+    .get((req, res) => {
+        // convert JSON code to string for readable to the network
+        res.json(books);
+
+    })
+
+    // add a book in the books list
+
+    .post((req, res) => {
+
+        // send 400 status if the user send bad request or invalid data
+        if (!req.body.title) {
+            return res.status(400).json({ error: 'title is missing' });
+        }
+
+        // create id for that book according 
+        const book_id = books.length + 1;
+
+        const book = {
+            id: book_id,
+            title: req.body.title,
+            author: req.body.author || 'Anonymous'  // if req.body.author is null, then add 'Anonymous'
+        }
+
+        // add book in the books list
+        books.push(book);
+
+        // send response with 201 status code ---> sth is created in the server side
+        res.status(201).json(book);
+    })
+
+    .put((req, res) => {
+        res.status(405).json({ "error": "PUT request is not allowed" });
+    })
+    .delete((req, res) => {
+        res.json({});
+    });
+
+
+// path for /api/books/:book_id
+router.route('/:book_id')
+
+    // get only specific book
+    .get((req, res) => {
+        // convert String bookId into int because data type of id is int in books list 
+        const bookId = Number(req.params.book_id);
+        const bookDetail = books.find((singleBook) => singleBook.id === bookId);
+        res.send(bookDetail);
+        // res.json(req.params);
+    })
+
+    // update particular book
+    .put((req, res) => {
+        // convert String id into number id
+        const bookId = Number(req.params.book_id);
+
+        // const book = books.find(singleBook => );
+        const updated_book = books.map(singleBook => {
+            if (singleBook.id === bookId) {
+                singleBook.title = req.body.title || singleBook.title;
+                singleBook.author = req.body.author || singleBook.author;
+
+            };
+            return singleBook;
+        });
+        // console.log(book);
+        res.status(201).json(updated_book);
+    })
+
+    // delete particular book
+    .delete((req, res) => {
+        const newBookList = books.filter(singleBook => {
+            if (singleBook.id != req.params.book_id) return singleBook;
+        });
+        res.json(newBookList);
+    })
+    .post((req, res) => {
+        res.status(405).json({ "error": "POST method is not allowed here" });
+    });
+
+
+// export it to use in other file
+module.exports = router;
